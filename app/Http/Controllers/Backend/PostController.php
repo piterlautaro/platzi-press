2b0ed441e3f4f74cd34c5472c8b5d2c7dcfe4730
Pedro Jiménez <47641247+piterlautaro@users.noreply.github.com>
2021-07-10 17:48:30 -0400

    :sparkles: Up post edit form

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -51,17 +51,6 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Post  $post
@@ -69,7 +58,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('posts.edit', compact('post'));
     }
 
     /**
